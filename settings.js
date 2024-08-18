5a2a376e6a3d3970edf5d2f36dc077617adf2b3d
newtonartemis2 <newtonartemis2@gmail.com>
2024-08-18 21:47:06 +0100

    Update settings.js

--- settings.js
+++ settings.js
@@ -10,8 +10,8 @@ const fs = require('fs')
 const chalk = require('chalk')
 
 //contact details
-global.ownernomer = "910000000000"
-global.ownername = "🐛Tech God"
+global.ownernomer = "2348125180317"
+global.ownername = "Artemis"
 global.ytname = "YT: Tech-God"
 global.socialm = "GitHub: techgod143"
 global.location = "India, Up, Rampur"
